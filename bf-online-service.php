61ded7e4da0676fe8d9dd3fd8c2711a5e36376fd
Taichi Maruyama <maruyama@breadfish.jp>
2021-06-04 11:16:06 +0900

    初回コミット

---
+++ bf-online-service.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Online Service
+Plugin URI:
+Description: オンライン礼拝を管理するプラグインです。
+Author:BREADFISH
+Author URI:http://breadfish.jp
+Version: 1.0.0
+*/
+
+/**
+ * BF_Online_Service class.
+ */
+class BF_Online_Service {
+	
+
+
+
+
+}
